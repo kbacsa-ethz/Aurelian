95a9bfb227230fad421d3768defbd93d31bdd65a
Kiran <kiran.bacsa@sec.ethz.ch>
2022-03-08 18:22:46 +0800

    added glad

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <GLFW/glfw3.h>
+#include "statemanager.h"
+
+#include "glad.c"
 
 // Debuggig macros
 #define PRINT_VAR(X) printf(#X " is %d at address %p\n", X, &X);
 #define SWAP(A, B) A ^= B ^= A ^= B;
 
-int main(int argc, char *argv[])
-{
-    printf("Hello world.");
+int main(int argc, char *argv[]) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Game Window", NULL, NULL);
+
+    if (window == NULL) {
+       printf("Failed to create GLFW window.\n");
+       glfwTerminate();
+       return -1;
+    }
+
+    glfwTerminate();
     return 0;
 }
