837ae29ffee9a2fa95f75796041eacd0d7cb6fa2
Kiran <kiran.bacsa@sec.ethz.ch>
2022-03-10 17:48:30 +0800

    render blue background

--- src/main.c
+++ src/main.c
@@ -8,20 +8,47 @@
 #define PRINT_VAR(X) printf(#X " is %d at address %p\n", X, &X);
 #define SWAP(A, B) A ^= B ^= A ^= B;
 
+#define WIDTH 800
+#define HEIGHT 600
+
 int main(int argc, char *argv[]) {
+
+    // Initialize GLFW library
     glfwInit();
+
+    // Indicate which version is used
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Use core (no backward compatibility)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Game Window", NULL, NULL);
+    // Create window
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Game Window", NULL, NULL);
 
+    // Quit if failed to create
     if (window == NULL) {
        printf("Failed to create GLFW window.\n");
        glfwTerminate();
        return -1;
     }
+
+    // Introduce window to current context
     glfwMakeContextCurrent(window);
+
+    // Load GLAD library
+    gladLoadGL();
+
+    // Select rendeing area
+    glViewport(0, 0, HEIGHT, WIDTH);
+
+    // Set color to blue
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    // Execute previous command
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Show next buffer
+    glfwSwapBuffers(window);
+
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
