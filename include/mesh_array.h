e19eb213645135536ba23dbfcc539cb977b92d6e
sbeuret <samuel.beuret@epfl.ch>
2022-05-14 09:41:30 +0200

    version of mesh arrays

---
+++ include/mesh_array.h
@@ -0,0 +1,8 @@
+//
+// Created by sbeuret on 14.05.22.
+//
+
+#ifndef AURELIAN_MESH_ARRAY_H
+#define AURELIAN_MESH_ARRAY_H
+
+#endif //AURELIAN_MESH_ARRAY_H
---
+++ include/mesh_arrays.h
@@ -0,0 +1,11 @@
+//
+// Created by sbeuret on 14.05.22.
+//
+
+#ifndef AURELIAN_MESH_ARRAYS_H
+#define AURELIAN_MESH_ARRAYS_H
+
+
+
+
+#endif //AURELIAN_MESH_ARRAYS_H
