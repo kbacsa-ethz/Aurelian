99b487a951503fbf9f597d75a056f36343a973f1
Kiran <kiran.bacsa@sec.ethz.ch>
2022-06-10 22:02:35 +0800

    remove readfile from SHADERS

--- src/shaders.c
+++ src/shaders.c
@@ -2,37 +2,15 @@
 #include <stdlib.h>
 #include <shaders.h>
 
+#include "utils.h"
+
 #define ERROR_LOG_LENGTH 1024
 
 
-char *SHADERS_readfile(const char *filename) {
-
-    // from https://stackoverflow.com/questions/3747086/reading-the-whole-text-file-into-a-char-array-in-c
-    char *shaderpointer;
-    long length;
-    FILE *f = fopen (filename, "rb");
-
-    if (f) {
-      fseek (f, 0, SEEK_END);
-      length = ftell (f);
-      fseek (f, 0, SEEK_SET);
-      shaderpointer = (char*)malloc ((length+1)*sizeof(char));
-      if (shaderpointer) {
-        fread (shaderpointer, sizeof(char), length, f);
-        *(shaderpointer+length) = '\0'; // Prevent garbage at the end of string.
-      }
-
-      fclose (f);
-    }
-
-    return shaderpointer;
-
-}
-
 GLuint SHADERS_initialize(char *vertexFilename, char *fragmentFilename) {
 
-    char *vertexShaderSource = SHADERS_readfile(vertexFilename);
-    char *fragmentShaderSource = SHADERS_readfile(fragmentFilename);
+    char *vertexShaderSource = UTILS_readfile(vertexFilename);
+    char *fragmentShaderSource = UTILS_readfile(fragmentFilename);
 
     // ADD CONDITION IF NULL THEN QUIT
     // printf("vertexShaderSource = %s\n", vertexShaderSource);
